5d7ef6d0e69e6fb56db41554c13140643c2a7fa9
mdaamer248 <mdaamer248@gmail.com>
2023-04-21 10:40:12 +0530

    webhook response body debugging

--- src/ico/ico.webhook.controller.ts
+++ src/ico/ico.webhook.controller.ts
@@ -1,46 +1,46 @@
 import {
-    Controller,
-    Post,
-    HttpCode,
-    HttpStatus,
-    Header,
-    Body,
-    Request,
-    Get,
-    Param,
-  } from '@nestjs/common';
-  import { ApiTags } from '@nestjs/swagger';
-  import { SubmitICOEthAddressDTO } from './dto/submit-ico-eth-address.dto';
-  import { ICOWebHookService } from './ico.webhook.service';
-  
-  @ApiTags('ICO/WEBHOOK')
-  @Controller('ico/webhook')
-  export class ICOWebhookController {
-    constructor(private webHookService: ICOWebHookService) {}
-    @Post('/order/completion')
-    @HttpCode(200)
-    async handleWebhook(@Body() body: any) {
-      if (body.event_type == 'CHECKOUT.ORDER.APPROVED') {
-        return await this.webHookService.checkoutOrderApproved(body);
-      }
-      if (body.event_type == 'PAYMENT.CAPTURE.COMPLETED') {
-        return await this.webHookService.paymentCaptureCompleted(body);
-      }
-    }
-  
-    @Post('/submit-eth-address')
-    async submitEthAddress(@Body() body: SubmitICOEthAddressDTO) {
-      const payment = await this.webHookService.submitEthAddress(
-        body.payPalOrderId,
-        body.ethAddress,
-      );
-      return payment;
-    }
+  Controller,
+  Post,
+  HttpCode,
+  HttpStatus,
+  Header,
+  Body,
+  Request,
+  Get,
+  Param,
+} from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { SubmitICOEthAddressDTO } from './dto/submit-ico-eth-address.dto';
+import { ICOWebHookService } from './ico.webhook.service';
 
-    @Get('claim/vadi-coins/:orderId')
-    async claimVadiCoinsByOrderId(@Param('orderId') orderId: string){
-      const tsx_hash = await this.webHookService.issueVadiCoins(orderId);
-      return tsx_hash;
+@ApiTags('ICO/WEBHOOK')
+@Controller('ico/webhook')
+export class ICOWebhookController {
+  constructor(private webHookService: ICOWebHookService) {}
+  @Post('/order/completion')
+  @HttpCode(200)
+  async handleWebhook(@Body() body: any) {
+    console.log(JSON.stringify(body));
+    if (body.event_type == 'CHECKOUT.ORDER.APPROVED') {
+      return await this.webHookService.checkoutOrderApproved(body);
+    }
+    if (body.event_type == 'PAYMENT.CAPTURE.COMPLETED') {
+      return await this.webHookService.paymentCaptureCompleted(body);
     }
   }
-  
+
+  @Post('/submit-eth-address')
+  async submitEthAddress(@Body() body: SubmitICOEthAddressDTO) {
+    const payment = await this.webHookService.submitEthAddress(
+      body.payPalOrderId,
+      body.ethAddress,
+    );
+    return payment;
+  }
+
+  @Get('claim/vadi-coins/:orderId')
+  async claimVadiCoinsByOrderId(@Param('orderId') orderId: string) {
+    const tsx_hash = await this.webHookService.issueVadiCoins(orderId);
+    return tsx_hash;
+  }
+}
